afa9160d74a3d42675bacb066f703abe04c4be58
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2023-12-14 17:46:20 -0300

    usando a promise

--- alura-cursos/CRUD com JS Assíncrono/admin/service/cliente-service.js
+++ alura-cursos/CRUD com JS Assíncrono/admin/service/cliente-service.js
@@ -28,54 +28,69 @@ const tabela = document.querySelector('[data-tabela]');
 // Agora temos o elemento pai (tbody) e o elemento filho que é a linha que acabamos de criar;
 // Para colocar ela dentro desse elemento pai utilizamos o appendChild criando o tabela.appendChild(criaNovaLinha(nome,email));
 
-// Os dados vão vir da const data. Então colocaremos essa parte do codigo dentro do metodo onload onde ela foi declarada e usaremos o forEacth() para percorrer cada um referente a cada um dos clientes em db.json;
+// Isso tudo acontecerá depois que a promisse for cumprida. A promisse estta dentro da função listaClientes, então será executado depois que chamarmos essa função ao fim do código. 
 
 
 
 // USANDO O OBJETO xmlhttprequest
-// PASSO A PASSO:
+        // PASSO A PASSO:
 
 // OBS: Antes de mais nada verificar se o JSON-SERVER está inicializado;
 
-// 1. Para fazer a comunicação entre nossa aplicação e a API usammos o objeto XMLHttpRequest;
-// 2. Inicializar esse objeto
+// OBS2: Primeiramente crio uma função para organizar o código. E dentro dessa função crio uma promisse. 
+const listaClientes = () => {
 
-const http = new XMLHttpRequest() 
+    //Inicializa a promisse 
+    const promise = new Promise((resolve, reject) => {
+        // O resolve e o reject vão lidar com o sucesso ou o erro da nossa chamada
 
-// 3. Abrir a comunicação entre minha aplicação e a API através do MÉTODO OPEN();
 
-http.open('GET', 'http://localhost:3000/profile');
-// O primeiro parâmetro é o que eu quero falar com o servidor (o que vou pedir a ele);
-// O segundo é o endereço para onde eu irei enviar a minha requisição;
-// MÉTODO GET (Para pegar os dados que estão no servidor)
+        // 1. Para fazer a comunicação entre nossa aplicação e a API usammos o objeto XMLHttpRequest;
+        // 2. Inicializar esse objeto
 
-// 4. Enviamos a nossa requisição através do médodo SEND();
-http.send()
+        const http = new XMLHttpRequest() 
 
-// 5. Indicamos ao JavaScript o que queremos qque aconteça depois que fizermos a requisição através do método ONLOAD (AO CARREGAR A PÁGINA);
+        // 3. Abrir a comunicação entre minha aplicação e a API através do MÉTODO OPEN();
 
-// Vou usar o http.response, que é o que o navegador me devolveu, e vou imprimir no console do começo. Vou guardá-lo na const data, para uso futuro;
+        http.open('GET', 'http://localhost:3000/profile');
+        // O primeiro parâmetro é o que eu quero falar com o servidor (o que vou pedir a ele);
+        // O segundo é o endereço para onde eu irei enviar a minha requisição;
+        // MÉTODO GET (Para pegar os dados que estão no servidor)
 
-http.onload = () => {
-    const data = JSON.parse(http.response) //Está retornando no console como texto, pois o http é um elemento de texto e não reconhece   como js válido. Por isso precisamos transformar essa resposta em um objeto JS com o JSON.parse().
+        // 4. Indicamos ao JavaScript o que queremos que aconteça depois que fizermos a requisição através do método ONLOAD (AO CARREGAR A PÁGINA);
 
-    data.forEach((elemento) => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
-        //Assim conseguimos pegar cada dado referente ao cliente.
-        // Assim tbm colocamos o elemento filho dentro do elemento pai (criando as tds dentro do tbody);
+        // Dentro da função call-back do onload, vou passar uma verificação para saber se deu tudo ok ou não com a nossa chamada. Se (http.status >= 400), ou seja, 400 já é a bad request, a requisição que não deu certo, acima disso também tem erros, são erros relacionados a cliente servidor.
+
+        http.onload = () => {
+            if(http.status >= 400) {
+                reject(JSON.parse(http.response))
+            } else {
+                resolve(JSON.parse(http.response))
+            }
+        }
+
+        // 5. Enviamos a nossa requisição através do médodo SEND();
+        http.send()
     });
+    // console.log(promise) só pra ver o que tem dentro da promisse
 
-    const http2 = new XMLHttpRequest()
-    http2.open('GET', 'http://localhost:3000/profile/semanaPassada')
-
-    http2.onload = () => {
-        ..
-    }
-
-    http2.send()
+    return promise
 }
+
 
 // Simulamos uma API para teste (mockar dados). Vai estar no arquivo package.json, onde estará listado como dependencia do projeto o json-server; 
 
 // Abro o terminal e faço um npm install para baixar as dependencias do projeto;
 // Para rodar o json-server faço o comando JSON-server --watch;
+
+
+listaClientes()
+.then( (data) => {
+    data.forEach((elemento) => {
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+        //Assim conseguimos pegar cada dado referente ao cliente.
+        // Assim tbm colocamos o elemento filho dentro do elemento pai (criando as tds dentro do tbody);
+    });
+})
+
+// O them é utilizado para lidar com o "sucesso" de uma promise;
