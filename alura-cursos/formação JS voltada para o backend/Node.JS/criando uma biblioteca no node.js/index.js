29f7efe53fd2fccfa9cd3303f283fb652c6bee78
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2023-12-27 17:20:05 -0300

    npm

--- alura-cursos/formação JS voltada para o backend/Node.JS/criando uma biblioteca no node.js/index.js
+++ alura-cursos/formação JS voltada para o backend/Node.JS/criando uma biblioteca no node.js/index.js
@@ -1,1 +1,6 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+// Para usarmos sintaxe de modulo de forma que funcione no node.js precisamos ir no packge.json e "avisar" que estamos usando a sintaxe de modulo.
+
+console.log(chalk.blue('Olá mundo!'));
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement).');
+console.log('Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.');
