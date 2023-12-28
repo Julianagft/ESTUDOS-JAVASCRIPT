892c16216186fb1219e18b697fed882f14295d98
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2023-12-28 17:24:23 -0300

    funções assíncronas

--- alura-cursos/formação JS voltada para o backend/Node.JS/criando uma biblioteca no node.js/index.js
+++ alura-cursos/formação JS voltada para o backend/Node.JS/criando uma biblioteca no node.js/index.js
@@ -1,6 +1,34 @@
-import chalk from 'chalk';
+import fs from 'fs'; // Como é uma biblioteca nativa do node não precisamos instalar, apenas importar;
+
+import chalk from 'chalk'
 // Para usarmos sintaxe de modulo de forma que funcione no node.js precisamos ir no packge.json e "avisar" que estamos usando a sintaxe de modulo.
 
-console.log(chalk.blue('Olá mundo!'));
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement).');
-console.log('Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.');
+function tratarErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+    // Todo objeto erro tem uma propriedade dentro dele que é o código do erro.
+}
+
+async function pegaArquivo(caminhoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const dados = await fs.promises.readFile(caminhoArquivo, encoding)
+        console.log(chalk.green(dados));    
+    } catch(erro) {
+        tratarErro(erro);
+    }
+    
+
+    // TRADUÇÃO: pegaArquivo é um função asíncrona e precisamos esperar (await) o retorno de uma parte específica do códico, no caso é  
+
+    // usando o método readFile 
+    // fs.readFile(caminhoArquivo, encoding, (erro, dados) => {
+    //     if(erro) {
+    //         tratarErro(erro);
+    //     }
+    //     console.log(chalk.green(dados));
+    // } )
+}
+
+pegaArquivo('./arquivos/texto.md');
+
+ 
